c752b5599827c2358088e534eba1dd982aa961cf
Martin Bernstorff <martinbernstorff@gmail.com>
2022-12-15 20:03:42 +0100

    feat: add correct calendar to description when syncing

--- src/output_services/gcal/syncer.py
+++ src/output_services/gcal/syncer.py
@@ -1,28 +1,27 @@
 from datetime import datetime
-from re import S
-from turtle import color
-from typing import Dict, List
+from typing import List
 
-import pandas as pd
 from gcsa.calendar import Calendar
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
-from pydantic import BaseModel
 from wasabi import Printer
 
+from output_services.events_to_calendar_mapper import EventsToCalendarMapper
 from utils.log import log
 
 log = Printer(timestamp=True)
 
 
 class CalendarSyncer:
-    def __init__(self):
+    def __init__(self, group_col_name: str = "category"):
         self.calendar = GoogleCalendar(
             "martinbernstorff@gmail.com",
             credentials_path="credentials/credentials.json",
         )
 
         self.timezone = self.calendar.get_settings().timezone
+        self.group_col_name = group_col_name
+        self.min_date_in_events = None
 
     def _deduplicate_events(
         self, events: List[Event], calendar: Calendar, events_in_calendar: List[Event]
@@ -37,6 +36,7 @@ class CalendarSyncer:
                     event.start == calendar_event.start
                     and event.end == calendar_event.end
                     and event.summary == calendar_event.summary
+                    and event.description == calendar_event.description
                     for calendar_event in events_in_calendar
                 ]
             )
@@ -60,6 +60,8 @@ class CalendarSyncer:
 
         matched_event = event_matches[0]
         event_matches[0].end = event_to_sync.end
+        event_matches[0].description = event_to_sync.description
+
         calendar.update_event(matched_event)
         log.good(
             f"Updated event in calendar, {matched_event.start} - {matched_event.summary}"
@@ -87,11 +89,11 @@ class CalendarSyncer:
     ) -> None:  # Add calendar arguments here
         calendar = self.calendar
 
-        min_date_in_events = min([event.start for event in events])
+        self.min_date_in_events = min([event.start for event in events])
 
         events_in_calendar = list(
             calendar.get_events(
-                min_date_in_events,
+                self.min_date_in_events,
                 datetime.today(),
                 order_by="updated",
                 single_events=True,
@@ -109,3 +111,8 @@ class CalendarSyncer:
                 events_in_calendar=events_in_calendar,
                 calendar=calendar,
             )
+
+        if self.group_col_name:
+            EventsToCalendarMapper(
+                gcal_account=self.calendar, min_date=self.min_date_in_events
+            ).move_events_to_correct_calendar()
