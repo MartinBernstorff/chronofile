1413ac8344f4482299d31e6dce7d8602c246c50c
Martin Bernstorff <martinbernstorff@gmail.com>
2023-03-01 13:46:53 +0100

    Update RescueTime Calendar API Key

--- update_rescuetime_calendar_api_key.sh
+++ update_rescuetime_calendar_api_key.sh
@@ -3,7 +3,7 @@ cd $DIR
 echo $PWD
 rm -rf credentials/token.pickle
 source .venv/bin/activate
-python src/update_rescuetime_calendar_api_key.py
+python src/update_google_token.py
 git add .
 git commit -m "Update RescueTime Calendar API Key"
 git push
