8d9f3bd64795e0d97a2468b11bb65635ffd746cd
Martin Bernstorff <martinbernstorff@gmail.com>
2024-05-11 11:47:50 +0200

    update 9 files and delete 4 files

---
+++ src/rescuetime_to_gcal/cli.py
@@ -0,0 +1,61 @@
+import logging
+from typing import Annotated
+
+import coloredlogs
+import pandas as pd
+import typer
+
+from rescuetime_to_gcal.config import config as cfg
+from rescuetime_to_gcal.input_services.rescuetime import Rescuetime
+from rescuetime_to_gcal.output_services.gcal.converter import df_to_gcsa_events
+from rescuetime_to_gcal.output_services.gcal.syncer import GcalSyncer
+
+log = coloredlogs.install(  # type: ignore
+    level="INFO",
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y/%m/%d %H:%M:%S",
+)
+
+app = typer.Typer()
+
+
+@app.command()
+def cli(
+    rescuetime_api_key: Annotated[str, typer.Argument(envvar="RESCUETIME_API_KEY")],
+    gcal_email: Annotated[str, typer.Argument(envvar="GCAL_EMAIL")],
+    gcal_project_id: Annotated[
+        str,
+        typer.Argument(envvar="GCAL_PROJECT_ID"),
+    ],
+    gcal_client_id: Annotated[
+        str,
+        typer.Argument(envvar="GCAL_CLIENT_ID"),
+    ],
+    gcal_client_secret: Annotated[
+        str,
+        typer.Argument(envvar="GCAL_CLIENT_SECRET"),
+    ],
+):
+    logging.info("Starting script")
+    rescuetime_data = Rescuetime(api_key=rescuetime_api_key).pull(
+        anchor_date=pd.Timestamp.today(),
+        lookbehind_distance=cfg.sync_window,
+        titles_to_exclude=cfg.exclude_titles,
+        titles_to_keep=None,
+        min_duration=cfg.min_duration,
+        allowed_gap_for_combining=cfg.merge_gap,
+        metadata=cfg.metadata_enrichment,
+    )
+    events = df_to_gcsa_events(rescuetime_data)
+
+    logging.info("Syncing events to calendar")
+    GcalSyncer(
+        gcal_email=gcal_email,
+        gcal_client_id=gcal_client_id,
+        gcal_project_id=gcal_project_id,
+        gcal_client_secret=gcal_client_secret,
+    ).sync_events_to_calendar(events)
+
+
+if __name__ == "__main__":
+    app()
--- src/rescuetime_to_gcal/output_services/gcal/syncer.py
+++ src/rescuetime_to_gcal/output_services/gcal/syncer.py
@@ -1,5 +1,8 @@
+import json
 import time
+from dataclasses import dataclass
 from datetime import datetime
+from pathlib import Path
 from typing import List
 
 import pandas as pd
@@ -35,11 +38,32 @@ class MinimalEvent(BaseModel):
         )
 
 
+@dataclass
 class GcalSyncer:
-    def __init__(self):
+    gcal_email: str
+    gcal_client_id: str
+    gcal_project_id: str
+    gcal_client_secret: str
+
+    def __post_init__(self):
+        credentials = {
+            "installed": {
+                "client_id": self.gcal_client_id,
+                "project_id": self.gcal_project_id,
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_secret": self.gcal_client_secret,
+                "redirect_uris": ["http://localhost"],
+            }
+        }
+
+        credentials_path = Path(__file__).parent.parent / "credentials.json"
+        credentials_path.write_text(json.dumps(credentials))
+
         self.calendar: GoogleCalendar = GoogleCalendar(
-            "martinbernstorff@gmail.com",
-            credentials_path="credentials/credentials.json",
+            self.gcal_email,
+            credentials_path=str(credentials_path),
         )  # type: ignore
 
         self.timezone = self.calendar.get_settings().timezone
