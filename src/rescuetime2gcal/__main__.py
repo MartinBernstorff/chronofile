6bb6efad77fef372884a86572a066aa4af196782
Martin Bernstorff <martinbernstorff@gmail.com>
2024-07-17 10:11:42 +0200

    update .ruff.toml, __main__.py and source_event.py Testing

--- src/rescuetime2gcal/__main__.py
+++ src/rescuetime2gcal/__main__.py
@@ -6,9 +6,12 @@ import devtools
 from iterpy.arr import Arr
 
 from rescuetime2gcal import delta
-from rescuetime2gcal.clients import gcal
 from rescuetime2gcal.config import config as cfg
-from rescuetime2gcal.preprocessing import DestinationEvent, merge_within_window, parse_events
+from rescuetime2gcal.preprocessing import (
+    DestinationEvent,
+    merge_within_window,
+    parse_events,
+)
 
 if TYPE_CHECKING:
     from rescuetime2gcal.clients.event_source import EventSource
@@ -19,7 +22,9 @@ log = logging.getLogger(__name__)
 
 
 def main(
-    input_clients: Sequence["EventSource"], destination_client: "DestinationClient", dry_run: bool
+    input_clients: Sequence["EventSource"],
+    destination_client: "DestinationClient",
+    dry_run: bool,
 ) -> None:
     input_events = Arr(input_clients).map(lambda f: f()).flatten().to_list()
 
@@ -45,9 +50,12 @@ def main(
 
 
 def pipeline(
-    source_events: Sequence["SourceEvent"], destination_events: Sequence["DestinationEvent"]
+    source_events: Sequence["SourceEvent"],
+    destination_events: Sequence["DestinationEvent"],
 ) -> Sequence[delta.EventChange]:
-    sufficient_length_events = Arr(source_events).filter(lambda e: e.duration > cfg.min_duration)
+    sufficient_length_events = Arr(source_events).filter(
+        lambda e: e.duration > cfg.min_duration
+    )
 
     parsed_events = sufficient_length_events.map(
         lambda e: parse_events(
@@ -57,7 +65,8 @@ def pipeline(
 
     filtered_by_title = parsed_events.filter(
         lambda e: not any(
-            excluded_title.lower() in e.title.lower() for excluded_title in cfg.exclude_titles
+            excluded_title.lower() in e.title.lower()
+            for excluded_title in cfg.exclude_titles
         )
     )
 
