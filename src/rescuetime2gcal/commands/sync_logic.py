a5436a6520de72251c460b950ace2ef6594ed12f
Martin Bernstorff <martinbernstorff@gmail.com>
2024-11-03 12:23:27 +0100

    fix: cli structure (#46)

--- src/rescuetime2gcal/commands/sync_logic.py
+++ src/rescuetime2gcal/commands/sync_logic.py
@@ -1,31 +1,20 @@
 import datetime
-import importlib
-import importlib.metadata
 import logging
-import time
 from dataclasses import dataclass
 from functools import partial
-from typing import TYPE_CHECKING, Annotated, Callable, Mapping, Optional, Sequence
+from typing import TYPE_CHECKING, Callable, Mapping, Optional, Sequence
 
 import coloredlogs
-import devtools
-import rich
-import rich.pretty
-import typer  # noqa: TCH002
 from iterpy.arr import Arr
 
 import rescuetime2gcal.diff as diff
-from rescuetime2gcal.commands.cli import app
-from rescuetime2gcal.config import Config, RecordCategory, RecordMetadata
-from rescuetime2gcal.destinations import gcal
 from rescuetime2gcal.event import DestinationEvent, hydrate_event
 from rescuetime2gcal.sources import activitywatch, rescuetime
-from rescuetime2gcal.sources.source import EventSource
 from rescuetime2gcal.sources.source_event import SourceEvent, WindowTitleEvent
 from rescuetime2gcal.timeline import merge_within_window
 
 if TYPE_CHECKING:
-    from rescuetime2gcal.sources.source import EventSource
+    from rescuetime2gcal.config import Config, RecordCategory, RecordMetadata
 
 coloredlogs.install(  # type: ignore
     level="INFO",
@@ -36,7 +25,7 @@ coloredlogs.install(  # type: ignore
 log = logging.getLogger(__name__)
 
 
-def _try_activitywatch(
+def try_activitywatch(
     activitywatch_base_url: str | None,
 ) -> Optional[Callable[[], Sequence[SourceEvent]]]:
     if activitywatch_base_url:
@@ -50,7 +39,7 @@ def _try_activitywatch(
     return None
 
 
-def _try_rescuetime(
+def try_rescuetime(
     rescuetime_api_key: str | None, cfg: "Config"
 ) -> Optional[Callable[[], Sequence[SourceEvent]]]:
     if rescuetime_api_key:
@@ -76,7 +65,7 @@ class DeduplicatedGroup:
         return DeduplicatedGroup(keeper=event_group[0], duplicates=[])
 
 
-def _pipeline(  # noqa: D417
+def pipeline(  # noqa: D417
     source_events: Sequence["SourceEvent"],
     destination_events: Sequence["DestinationEvent"],
     min_duration: "datetime.timedelta",
@@ -137,92 +126,3 @@ def _pipeline(  # noqa: D417
     changeset = diff.diff(merged_within_gap, destination_keepers)
 
     return [*changeset, *[diff.DeleteEvent(event=e) for e in destination_duplicates]]
-
-
-@app.command()
-def sync(
-    rescuetime_api_key: Annotated[Optional[str], typer.Argument(envvar="RESCUETIME_API_KEY")],
-    activitywatch_base_url: Annotated[
-        Optional[str], typer.Argument(envvar="ACTIVITYWATCH_BASE_URL")
-    ],
-    gcal_email: Annotated[str, typer.Argument(envvar="GCAL_EMAIL")],
-    gcal_client_id: Annotated[str, typer.Argument(envvar="GCAL_CLIENT_ID")],
-    gcal_client_secret: Annotated[str, typer.Argument(envvar="GCAL_CLIENT_SECRET")],
-    gcal_refresh_token: Annotated[str, typer.Argument(envvar="GCAL_REFRESH_TOKEN")],
-    config_path: Annotated[str, typer.Argument(envvar="CONFIG_PATH")] = "config.toml",
-    dry_run: bool = False,
-    watch: Annotated[bool, typer.Option(envvar="WATCH")] = False,
-):
-    cfg = Config.from_toml(config_path)
-
-    logging.info(f"Running rescuetime2gcal version {importlib.metadata.version('rescuetime2gcal')}")
-    logging.info(rich.pretty.pprint(cfg))
-    logging.info("Starting sync")
-
-    event_sources: Sequence[EventSource] = [
-        s
-        for s in [
-            _try_rescuetime(rescuetime_api_key, cfg),
-            _try_activitywatch(activitywatch_base_url),
-        ]
-        if s is not None
-    ]
-
-    if len(event_sources) == 0:
-        raise ValueError(
-            "No event sources provided. Specify either rescuetime_api_key or activitywatch_base_url"
-        )
-
-    input_events = Arr(event_sources).map(lambda f: f()).flatten().to_list()
-
-    destination_client = gcal.GcalClient(
-        calendar_id=gcal_email,
-        client_id=gcal_client_id,
-        client_secret=gcal_client_secret,
-        refresh_token=gcal_refresh_token,
-    )
-
-    changes = _pipeline(
-        source_events=input_events,
-        destination_events=destination_client.get_events(
-            start=min([event.start for event in input_events]),
-            end=max([event.start for event in input_events])
-            + max([event.duration for event in input_events]),
-        ),
-        min_duration=cfg.min_duration,
-        category2emoji=cfg.category2emoji,
-        exclude_titles=cfg.exclude_titles,
-        merge_gap=cfg.merge_gap,
-        metadata_enrichment=cfg.metadata_enrichment,
-        exclude_apps=cfg.exclude_apps,
-    )
-
-    logging.info(f"Changes to be made {devtools.debug.format(changes)}")
-
-    if not dry_run:
-        log.info("Dry-run is false, syncing changes")
-        for change in changes:
-            match change:
-                case diff.NewEvent():
-                    destination_client.add_event(change.event)
-                case diff.UpdateEvent():
-                    destination_client.update_event(change.event)
-                case diff.DeleteEvent():
-                    destination_client.delete_event(change.event)
-    else:
-        log.info("Dry-run enabled, skipping sync")
-
-    if watch:
-        sleep_minutes = 5
-        log.info(f"Watch is {watch}, sleeping for {sleep_minutes} minutes")
-        time.sleep(sleep_minutes * 60)
-        sync(
-            rescuetime_api_key=rescuetime_api_key,
-            activitywatch_base_url=activitywatch_base_url,
-            gcal_email=gcal_email,
-            gcal_client_id=gcal_client_id,
-            gcal_client_secret=gcal_client_secret,
-            gcal_refresh_token=gcal_refresh_token,
-            dry_run=dry_run,
-            watch=watch,
-        )
---
+++ src/rescuetime2gcal/main.py
@@ -0,0 +1,118 @@
+import importlib.metadata
+import logging
+import time
+from typing import TYPE_CHECKING, Annotated, Optional, Sequence
+
+import devtools
+import rich.pretty
+import typer
+from iterpy.arr import Arr
+
+import rescuetime2gcal.diff as diff
+from rescuetime2gcal.commands.sync_logic import log, pipeline, try_activitywatch, try_rescuetime
+from rescuetime2gcal.config import Config
+from rescuetime2gcal.destinations import gcal
+from rescuetime2gcal.destinations.gcal.auth import print_refresh_token
+
+if TYPE_CHECKING:
+    from rescuetime2gcal.sources.source import EventSource
+
+app = typer.Typer()
+
+
+@app.command()
+def sync(
+    rescuetime_api_key: Annotated[Optional[str], typer.Argument(envvar="RESCUETIME_API_KEY")],
+    activitywatch_base_url: Annotated[
+        Optional[str], typer.Argument(envvar="ACTIVITYWATCH_BASE_URL")
+    ],
+    gcal_email: Annotated[str, typer.Argument(envvar="GCAL_EMAIL")],
+    gcal_client_id: Annotated[str, typer.Argument(envvar="GCAL_CLIENT_ID")],
+    gcal_client_secret: Annotated[str, typer.Argument(envvar="GCAL_CLIENT_SECRET")],
+    gcal_refresh_token: Annotated[str, typer.Argument(envvar="GCAL_REFRESH_TOKEN")],
+    config_path: Annotated[str, typer.Argument(envvar="CONFIG_PATH")] = "config.toml",
+    dry_run: bool = False,
+    watch: Annotated[bool, typer.Option(envvar="WATCH")] = False,
+):
+    cfg = Config.from_toml(config_path)
+
+    logging.info(f"Running rescuetime2gcal version {importlib.metadata.version('rescuetime2gcal')}")
+    logging.info(rich.pretty.pprint(cfg))
+    logging.info("Starting sync")
+
+    event_sources: Sequence[EventSource] = [
+        s
+        for s in [
+            try_rescuetime(rescuetime_api_key, cfg),
+            try_activitywatch(activitywatch_base_url),
+        ]
+        if s is not None
+    ]
+
+    if len(event_sources) == 0:
+        raise ValueError(
+            "No event sources provided. Specify either rescuetime_api_key or activitywatch_base_url"
+        )
+
+    input_events = Arr(event_sources).map(lambda f: f()).flatten().to_list()
+
+    destination_client = gcal.GcalClient(
+        calendar_id=gcal_email,
+        client_id=gcal_client_id,
+        client_secret=gcal_client_secret,
+        refresh_token=gcal_refresh_token,
+    )
+
+    changes = pipeline(
+        source_events=input_events,
+        destination_events=destination_client.get_events(
+            start=min([event.start for event in input_events]),
+            end=max([event.start for event in input_events])
+            + max([event.duration for event in input_events]),
+        ),
+        min_duration=cfg.min_duration,
+        category2emoji=cfg.category2emoji,
+        exclude_titles=cfg.exclude_titles,
+        merge_gap=cfg.merge_gap,
+        metadata_enrichment=cfg.metadata_enrichment,
+        exclude_apps=cfg.exclude_apps,
+    )
+
+    logging.info(f"Changes to be made {devtools.debug.format(changes)}")
+
+    if not dry_run:
+        log.info("Dry-run is false, syncing changes")
+        for change in changes:
+            match change:
+                case diff.NewEvent():
+                    destination_client.add_event(change.event)
+                case diff.UpdateEvent():
+                    destination_client.update_event(change.event)
+                case diff.DeleteEvent():
+                    destination_client.delete_event(change.event)
+    else:
+        log.info("Dry-run enabled, skipping sync")
+
+    if watch:
+        sleep_minutes = 5
+        log.info(f"Watch is {watch}, sleeping for {sleep_minutes} minutes")
+        time.sleep(sleep_minutes * 60)
+        sync(
+            rescuetime_api_key=rescuetime_api_key,
+            activitywatch_base_url=activitywatch_base_url,
+            gcal_email=gcal_email,
+            gcal_client_id=gcal_client_id,
+            gcal_client_secret=gcal_client_secret,
+            gcal_refresh_token=gcal_refresh_token,
+            dry_run=dry_run,
+            watch=watch,
+        )
+
+
+@app.command()
+def gcal_auth(
+    gcal_client_id: Annotated[str, typer.Argument(envvar="GCAL_CLIENT_ID")],
+    gcal_client_secret: Annotated[str, typer.Argument(envvar="GCAL_CLIENT_SECRET")],
+):
+    logging.info("Getting refresh token")
+    print_refresh_token(client_id=gcal_client_id, client_secret=gcal_client_secret)
